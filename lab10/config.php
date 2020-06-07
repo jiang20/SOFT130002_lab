6e2eb55e54cb1832857ad2157862778fb0271929
jiang20 <2833306508@qq.com>
2020-06-07 21:06:35 +0800

    finish lab10

---
+++ lab10/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'testUser');
+define('DBPASS', 'jj200004');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
+?>
